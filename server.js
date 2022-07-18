dba2d93a60962362c0c98fccf45c71f48e7374c2
Son Vo <vnsonvo@gmail.com>
2022-07-18 20:39:14 +0700

    connect to DB

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
-// mongoose.connect();
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true }, () => {
+  console.log(`Connected to DB`);
+});
+
+// middleware
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
